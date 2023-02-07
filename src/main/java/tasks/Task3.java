b2648da6ea153396344c2cf9113a1b2874438871
Viktor Egorov <viktor.spoon2@gmail.com>
2023-02-07 22:10:25 +0400

    Task3 completed

---
+++ src/main/java/tasks/Task3.java
@@ -0,0 +1,14 @@
+package tasks;
+//Very simple, given an integer or a floating-point number, find its opposite.
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int opposite(int number) {
+        number = number*(-1);
+        return number;
+    }
+}
+
+
