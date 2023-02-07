c6e36e5c2e20b789eb2c1bd1c899b22defe1c3d9
Viktor Egorov <viktor.spoon2@gmail.com>
2023-02-07 21:40:10 +0400

    Task1 complited

---
+++ src/main/java/tasks/Task1.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] answer = countBy(2, 5);
+        for (int i : answer
+        ) {
+            System.out.print(i);
+        }
+    }
+
+    public static int[] countBy(int x, int n) {
+        int count = x;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = count;
+            count += x;
+        }
+        return answer;
+    }
+}
+
