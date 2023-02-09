2c2253ffa570adf86d1503ca945854fdb782cdc6
Viktor Egorov <viktor.spoon2@gmail.com>
2023-02-10 02:20:51 +0400

    Task2 completed

---
+++ src/main/java/tasks_7kyu/Task2.java
@@ -0,0 +1,10 @@
+package tasks_7kyu;
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+    public static String boolToWord(boolean b){
+        return b? "Yes":"No";
+    }
+}
