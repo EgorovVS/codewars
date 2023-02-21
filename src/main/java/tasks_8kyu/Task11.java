120c13d34afe6227fff44019098a1767768eb359
Viktor Egorov <viktor.spoon2@gmail.com>
2023-02-22 01:56:31 +0400

    Task11 completed

---
+++ src/main/java/tasks_8kyu/Task11.java
@@ -0,0 +1,11 @@
+package tasks_8kyu;
+//Implement a function which convert the given boolean value into its string representation.
+
+public class Task11 {
+    public static void main(String[] args) {
+
+    }
+    public static String convert(boolean b) {
+        return Boolean.toString(b);
+    }
+}
