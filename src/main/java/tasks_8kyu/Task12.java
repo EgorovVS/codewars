d5d0ce9f9afd5615a00edfd287747591098b35af
Viktor Egorov <viktor.spoon2@gmail.com>
2023-02-28 02:17:07 +0400

    task12 completed

---
+++ src/main/java/tasks_8kyu/Task12.java
@@ -0,0 +1,11 @@
+package tasks_8kyu;
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+public class Task12 {
+    public static void main(String[] args) {
+
+    }
+
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+}
